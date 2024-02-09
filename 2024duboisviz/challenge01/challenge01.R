11f505d3b975b91d096a3c4e0f74614cae9a8960
butames <72810726+butames@users.noreply.github.com>
2024-02-09 01:39:29 -0800

    Minor changes

--- 2024duboisviz/challenge01/challenge01.R
+++ 2024duboisviz/challenge01/challenge01.R
@@ -30,19 +30,24 @@ gashp <- st_read("data/georgia-1880-county-shapefile/DuBoisChallenge - Georgia C
 # DEFINE COLORS -----------------------------------------------------------
 
 
-navy <- "#392859"
-brwn <- "#BE9C80"
-darb <- "#6B442C"
-wine <- "#CD2A4A"
 
-pink <- "#E09991"
-yell <- "#E9B455"
-gree <- "#566859"
+colorsa <- c("#392859", "#BE9C80", "#6B442C")
+
+colorsb <- c("#CD2A4A", "#E09991", "#E9B455", "#566859")
 
 backgrd <- "#e4cfbd"
 textcl <- "#3e2b22"
 
-# DEFINE FONT -----------------------------------------------------------
+# ADD FONTS ----------------------------------------------------------------
+font_add_google(name = "Exo 2", family = "Exo 2")
+font <- "Exo 2"
+
+font_add_google(name = "Overpass", family = "Overpass")
+font_s <- "Overpass"
+
+# TURN ON SHOWTEXT --------------------------------------------------------
+showtext_auto()
+showtext_opts(dpi = 320)
 
 #Libre Franklin
 # Advent Pro
@@ -51,14 +56,71 @@ textcl <- "#3e2b22"
 
 # PLOT MAP ----------------------------------------------------------- 
 
-ggplot() + 
+plt1 <- ggplot() + 
   geom_sf(
     data = gashp,
-    color = "#b3cbdc")
+    color = "#483c32",
+    alpha = 0.9,
+    linewidth) +
+plt1
 
 # CREATE LEGEND  ----------------------------------------------------------- 
+# There are two legends in the WEB Dubois plate
+# I wondered how to split up the legend the way it is done on the original plate
+# But you can simply create the legend separately and paste it to 
 
+lg1 <- tibble(x = 0,
+              y = c(1,2,3),
+              label = c("10,000 TO 15,000",
+                        "15,000 TO 20,000",
+                        "BETWEEN 20,000 AND 30,000")) 
 
+lg2 <- tibble(x = 0,
+              y = c(4,3,2,1),
+              label = c("5,000 TO 10,000",
+                        "2,500 TO 5,000",
+                        "1000 TO 2,500",
+                        "UNDER 1,000"))
+
+# PLOT LEGENDS ----------------------------------------------------------- 
+# There are two legends in the WEB Dubois plate
+
+plt3 <- lg1 %>%
+  ggplot(aes(x,y)) +
+  geom_point(aes(fill = label),
+             shape = 21,
+             stroke = 0.1,
+             size = 10,
+             show.legend = FALSE)+
+  scale_fill_manual(values = colorsa, 
+                    breaks = c("10,000 TO 15,000",
+                               "15,000 TO 20,000",
+                               "BETWEEN 20,000 AND 30,000")) +
+  coord_cartesian(xlim = c(-0.2, 7),
+                  ylim = c(-1, 4)) +
+  theme_void() +
+  theme(plot.background = element_rect(color = backgrd, fill = backgrd),
+        panel.background = element_rect(color= backgrd, fill = backgrd))
+plt3
+
+plt4 <- lg2 %>%
+  ggplot(aes(x,y)) +
+  geom_point(aes(fill = label),
+             shape = 21,
+             stroke = 0.1,
+             size = 10,
+             show.legend = FALSE)+
+  scale_fill_manual(values = colorsb, 
+                    breaks = c("5,000 TO 10,000",
+                               "2,500 TO 5,000",
+                               "1000 TO 2,500",
+                               "UNDER 1,000")) +
+  coord_cartesian(xlim = c(-0.2,1),
+                  ylim = c(-0,5)) +
+  theme_void() +
+  theme(plot.background = element_rect(color = backgrd, fill = backgrd),
+        panel.background = element_rect(color= backgrd, fill = backgrd))
+plt4
 
 # SAVE MAP ----------------------------------------------------------- 
 
