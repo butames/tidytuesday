dc96b67b44d627293ba3401ca109048415db2244
butames <72810726+butames@users.noreply.github.com>
2024-03-01 01:05:59 -0800

    Changed README

--- 2024/20240220wk08/20240220wk08.R
+++ 2024/20240220wk08/20240220wk08.R
@@ -22,17 +22,31 @@ showtext_auto()
 showtext_opts(dpi = 320, regular.wt = 300, bold.wt = 800)
 
 ## LOAD FONTS -----------------------------------------------------------
-font_add("fa6-brands", "fonts/6.4.2/Font Awesome 6 Brands-Regular-400.otf") #Enables symbols such as twitter etc.
+#Enables symbols such as twitter etc.
+
+font_add("fa6-brands", "Users/seyramb/Library/Fonts/Font Awesome 6 Brands-Regular-400.otf") 
 font_add_google("Roboto Condensed", "title")
 font_add_google("Roboto Condensed", "subtitle")
 font_add_google("Roboto Condensed", "caption")
+font_add_google("Roboto Condensed", "text")
+
 
 ## DEFINE TITLES & CAPTION -----------------------------------------------------------
 
+
+
 tt <- str_glue("TidyTuesday: { 2024 } Week { 08 } &vert; Source: R Consortium Infrastructure Steering Committee (ISC) <br>")
-X  <- str_glue("<span style='font-fmaily: fa6-brands'>&#xe61b;</span>")
-gh <- str_glue("<span style='font-fmaily: fa6-brands'>&#xf09b;</span>")
-mn <- str_glue("<span style='font-fmaily: fa6-brands'>&#xf4f6;</span>")
+X  <- str_glue("<span style='font-family:fa6-brands'>&#xe61b;</span>")   
+gh <- str_glue("<span style='font-family:fa6-brands'>&#xf09b;</span>")
+mn <- str_glue("<span style='font-family:fa6-brands'>&#xf4f6;</span>")
+
+ttltxt <- str_glue("Bubble Chart Showing Grants Awarded")
+subtxt <- str_glue("R Consortium Steering Committee (ISC) Grants as a Function of Time and Amount")
+captxt <- str_glue("{tt} Visualization: {X} @butames &bull; {mn} @butames(mastodon.cloud) Code: {gh} butames &bull; Tools: #rstats #ggplot2")
+
+## DEFINE COLORS -----------------------------------------------------------
+
+palette <- c("#278B9AFF", "#590514FF", "#B50A2AFF")
 
 ## DATA WRANGLING -----------------------------------------------------------
 
@@ -40,15 +54,86 @@ df1 <- isc_grants %>%
   clean_names() %>%
   group_by(year, group) %>%
   summarise(total = sum(funded, rm.na = TRUE),
-            count = n())
+            count = n()) %>%
+  mutate(fillgroup = case_when(
+    group == "1" ~ palette[1],
+    group == "2" ~ palette[2],
+    TRUE ~ palette[3]  # Default value for other groups (optional)
+  ))
+
+## DEFINE THEME/AESTHETICS -----------------------------------------------------------
+
+bkgcol <- "#F4E3D3FF"
+
+ttlcol <- "#85898AFF"
+subcol <- "#85898AFF"
+capcol <- "#85898AFF"
+txtcol <- "#85898AFF"
+
+theme_set(theme_minimal(base_size = 12, base_family = "text"))
+theme_update(
+  plot.title.position = "plot",
+  plot.caption.position = "plot",
+  axis.title.x = element_text(margin = margin(t = 10, r = 0, b = 0, l = 0), size = rel(1), color = txtcol, family = "text", face = "bold"),
+  axis.title.y = element_text(margin = margin(t = 0, r = 10, b = 0, l = 0), size = rel(1), color = txtcol, family = "text", face = "bold"),
+  axis.text = element_text(size = rel(1), color = txtcol, family = "text"),
+  axis.line.x = element_blank(),
+  panel.grid.minor.y = element_blank(),
+  panel.grid.major.y = element_blank(),
+  panel.grid.minor.x = element_blank(),
+  panel.grid.major.x = element_blank(),
+  plot.margin = margin(t = 10, r = 10, b = 10, l = 10),
+  plot.background = element_rect(fill = bkgcol, color = bkgcol),
+  panel.background = element_rect(fill = bkgcol, color = bkgcol)
+)
+
 
 ## CREATE PLOT -----------------------------------------------------------
 
 plt1 <- df1 %>%
-  ggplot(aes(x = year, y = total, size = count, color = group, fill = group )) +
-  geom_point(alpha = 0.5, shape = 21, show.legend = T) +
-  scale_size(range = c(.1, 16), name = "Total Awardees") +
-  scale_fill_viridis(discrete=F, option="A")
+  ggplot() +
+  geom_point(aes(x = year, y = total, size = count, color = group, fill = fillgroup), alpha = 0.8, shape = 21, show.legend = T) +
+  scale_y_continuous(limits=c(10000,200000)) +
+  scale_x_continuous(limits=c(2014,2024)) +
+  scale_size(range = c(.1, 24), name = "Total Awardees", guide = "none") +
+  # scale_fill_manual(values = group, labels = c("Spring", "Fall"), name = "Award Cycle"))  
+  guides(color = guide_legend("Award Cycle")) +
+  guides(color = "none") +
+  labs(x = "",
+       y = "Grant Award Amount (USD)",
+       fill = "Award Cycle",
+       title = ttltxt,
+       subtitle = subtxt,
+       caption = captxt) 
+  theme(
+    plot.title = element_markdown(
+      size    = rel(1.5),
+      family  = "title",
+      color   = ttlcol,
+      hjust   = 0.5,
+      halign  = 0.5,
+      margin  = margin(t = 5, b = 5)
+    ),
+    plot.subtitle = element_markdown(
+      size    = rel(1.5),
+      family  = "subtitle",
+      color   = ttlcol,
+      hjust   = 0.5,
+      halign  = 0.5,
+      margin  = margin(t = 5, b = 5)
+    ),
+    plot.caption = element_markdown(
+      size    = rel(1),
+      family  = "caption",
+      color   = ttlcol,
+      hjust   = 0.5,
+      halign  = 0.5,
+      margin  = margin(t = 5, b = 5)
+    )
+  )
 
 plt1
+
+ggsave(paste0("bubblechart_", format(Sys.time(), "%d%m%Y"), ".png"), dpi = 320, height = 8.5, width = 11, limitsize = F)
+
 
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# TidyTuesdaySAB
-This repository presents an opportunity to play around with Tidy Tuesday data and create data visualizations. Presently, I intend to duplicate some of the more exciting visuals I have seen on the internet and perhaps use my own flare. With time and practice, I hope I, too, will become a data viz wiz. 
+<h1 style="font-weight:normal" align="center">
+  &nbsp;#Experimenting with TidyTuesday&nbsp;
+</h1>
+
+This repository presents an opportunity to play around with TidyTuesday data and to create data visualizations, using R/ R Studio and various data viz packages. Presently, I intend to duplicate some of the more exciting visuals I have seen on the internet using my own flare. With time and practice, I hope I, too, will become a data viz wiz. Occassionally, I will try to veer into other data viz creation tools such as D3 and Tableau. I have already used Tableau to some extent and am onversant with it. Who knows, if there is time on my hands (I doubt it) I will pick up Python.
+
+<div align="center">
+    
+&nbsp;&nbsp;&nbsp;
+
+[![GitHub Badge](https://img.shields.io/badge/Medium-12100E?style=for-the-badge&logo=medium&logoColor=white)](https://github.com/butames)
+[![Mastodon Badge](https://img.shields.io/badge/Gmail-D14836?style=for-the-badge&logo=gmail&logoColor=white)](https://mastodon.cloud/@butames)
+[![Linkedin Badge](https://img.shields.io/badge/linkedin-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://linkedin.com/in/butames)
+[![Twitter Badge](https://img.shields.io/badge/twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white)](https://twitter.com/butames)
+
+</div>
